d2ad6414ddd7b3166d9a86e312d2a75a983a0b21
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2023-07-08 17:56:24 -0400

    Setup backend server

---
+++ Server/database.js
@@ -0,0 +1,21 @@
+const mysql2 = require ('mysql2');
+
+var connection = new mysql2.createConnection({
+	host: process.env.HOST,
+	port: process.env.PORT,
+	database: process.env.DATABASE_NAME,
+	user: process.env.USERNAME,
+	password: process.env.PASSWORD,
+	multipleStatements: true
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.log ('Could not connect');
+    throw err;
+  } else {
+    console.log ('Connected to database.');
+  }
+});
+
+module.exports = connection;
--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+
 const express = require('express')
-const app = express()
-const port = 3000
+const connection = require('./database');
+const UserRoutes = require('./src/UserRoutes');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+UserRoutes(app, connection);
+
+app.listen(process.env.APIPORT, () => {
+  console.log(`Application listening on port ${process.env.APIPORT}`)
+});
